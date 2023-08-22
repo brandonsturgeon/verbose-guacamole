f4ad2d865b642aea79553accc0f243941dccf5f0
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-08-21 18:16:37 -0700

    Loop changes

--- change.sh
+++ change.sh
@@ -7,21 +7,28 @@ LINES=$(wc -l < "$FILE")
 # Randomly choose to add, modify, or delete
 ACTION=$((RANDOM % 3))
 
-case $ACTION in
-0) # Add a random line
-  echo "Random addition" >> "$FILE"
-  ;;
-1) # Modify a random line
-  if [ $LINES -gt 0 ]; then
-    LINE_TO_MODIFY=$((RANDOM % LINES + 1))
-    awk -v line="$LINE_TO_MODIFY" 'NR==line{$0="Random modification"}1' "$FILE" > "$TEMP_FILE"
-    mv "$TEMP_FILE" "$FILE"
-  fi
-  ;;
-2) # Delete a random line
-  if [ $LINES -gt 0 ]; then
-    LINE_TO_DELETE=$((RANDOM % LINES + 1))
-    sed -i "${LINE_TO_DELETE}d" "$FILE"
-  fi
-  ;;
-esac
+change() {
+    case $ACTION in
+    0) # Add a random line
+      echo "Random addition" >> "$FILE"
+      ;;
+    1) # Modify a random line
+      if [ $LINES -gt 0 ]; then
+        LINE_TO_MODIFY=$((RANDOM % LINES + 1))
+        awk -v line="$LINE_TO_MODIFY" 'NR==line{$0="Random modification"}1' "$FILE" > "$TEMP_FILE"
+        mv "$TEMP_FILE" "$FILE"
+      fi
+      ;;
+    2) # Delete a random line
+      if [ $LINES -gt 0 ]; then
+        LINE_TO_DELETE=$((RANDOM % LINES + 1))
+        sed -i "${LINE_TO_DELETE}d" "$FILE"
+      fi
+      ;;
+    esac
+}
+
+NUM_CHANGES=35
+for ((i=0; i<$NUM_CHANGES; i++)); do
+    change
+done
