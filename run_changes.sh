528ff12856b3c8b9c9074b4454ee5405e7abf9e4
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-08-23 15:52:46 -0700

    Change 0 Wed 23 Aug 2023 03:52:46 PM PDT - head-c500devurandom

--- run_changes.sh
+++ run_changes.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 random_number=$(( ( RANDOM % 5 )  + 1 ))
+random_text() {
+    echo head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | fold -w 75 | head -n 1
+}
 
 for ((i=0; i<$random_number; i++)); do
     ./change.sh
     git add .
-    git commit -m "Change $i $(date) $(date) $(dd if=/dev/urandom bs=8 count=5)"
+    git commit -m "Change $i $(date) - $(random_text)"
 done
 
 git push origin main
